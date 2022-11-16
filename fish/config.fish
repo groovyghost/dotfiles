701dd96d898dac550710a1ffbb0c5d79735e97fd
groovyghost <grovyghost@protonmail.com>
2022-11-16 13:40:43 +0530

    Locale change

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 #! /bin/sh
+export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 set fish_greeting
 export EDITOR=nvim || export EDITOR=vim
 source $HOME/.config/fish/shortcuts.fish
@@ -12,3 +13,6 @@ set fish_color_valid_path --bold
 alias la='ls -lha'
 alias df='df -h'
 alias du='du -ch'
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/user/.local/bin/google-cloud-sdk/path.fish.inc' ]; . '/home/user/.local/bin/google-cloud-sdk/path.fish.inc'; end
