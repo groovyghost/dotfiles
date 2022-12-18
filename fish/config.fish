118be86bb82ceabe5f1ba28812f5909005a27e6b
groovyghost <grovyghost@protonmail.com>
2022-12-18 14:44:11 +0530

    feat: alias handle

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,12 @@ source $HOME/.config/fish/colors.fish
 set -gx PATH $HOME/.local/bin $HOME/.local/bin/go/bin /usr/local/bin $PATH
 set fish_color_valid_path --bold
 
-alias ls='exa -l --icons --git'
-alias ll='exa -l --icons -a --git'
+set -l exa_available (which exa)
+if test -n "$exa_available"
+    alias ls='exa --icons --git'
+    alias ll='exa -l --icons -a --git'
+end
+
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/user/.local/bin/google-cloud-sdk/path.fish.inc' ]; . '/home/user/.local/bin/google-cloud-sdk/path.fish.inc'; end
--- fish/shortcuts.fish
+++ fish/shortcuts.fish
@@ -8,3 +8,4 @@ abbr reading 'python3 /home/user/Public/Personal/read/source/main.py && notify-s
 abbr fvpn 'sudo openfortivpn'
 abbr rsync 'rsync -avzhP'
 abbr fcd 'cd (find * -type d | fzf)'
+abbr pip_upgrade_all 'pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U'
