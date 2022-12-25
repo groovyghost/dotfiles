36e0cdc938f46fcd80fbaa59266bb64a6c72184c
groovyghost <grovyghost@protonmail.com>
2022-12-25 14:34:44 +0530

    fix: transparent bg

--- nvim/lua/user/colors.lua
+++ nvim/lua/user/colors.lua
@@ -12,8 +12,7 @@ end
 
 vim.g.nord_contrast = true
 vim.g.nord_borders = true
-vim.g.nord_disable_background = false
-vim.g.nord_italic = true
+vim.g.nord_disable_background = true
 vim.g.nord_uniform_diff_background = true
 vim.g.nord_bold = false
 
