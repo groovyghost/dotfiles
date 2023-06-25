d82a6ba276c1ef0e3dc78d269449c66af77420e7
groovyghost <grovyghost@protonmail.com>
2023-06-25 19:33:16 +0530

    refactor: abbr refactor

--- fish/shortcuts.fish
+++ fish/shortcuts.fish
@@ -4,6 +4,6 @@ abbr termconf "$EDITOR ~/.config/alacritty/"
 abbr mkdir 'mkdir -p'
 abbr wrk 'cd /home/user/Public/Work'
 abbr psl ' cd /home/user/Public/Personal'
-abbr fvpn 'sudo openfortivpn'
+abbr fvpn 'sudo openfortivpn --persistent=2'
 abbr rsync 'rsync -avzhP'
 abbr fcd 'cd (find * -type d | fzf)'
